0626af5689f5e4529564bccab9523315c35c8ce4
LucianoVelasquez <125805986+LucianoVelasquez@users.noreply.github.com>
2024-04-20 10:29:22 -0300

    a

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex justify-center items-center w-full h-screen gap-x-5 bg-secondary-foreground">
       <Button>Test 1</Button>
-      <Button variant={"destructive"}>Test 1</Button>
-      <Button variant={"ghost"}>Test 1</Button>
-      <Button variant={"outline"}>Test 1</Button>
-      <Button variant={"secondary"}>Test 1</Button>
+      <Button variant={"destructive"} size={"lg"}>Test 1</Button>
+      <Button variant={"ghost"} size={"lg"}>Test 2</Button>
+      <Button variant={"outline"} size={"lg"}>Test 3</Button>
+      <Button variant={"secondary"} size={"lg"}>Test 4</Button>
     </div>
   );
 }
