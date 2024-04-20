5043edeefb6d073e44416cc7e478488d5db57650
LucianoVelasquez <125805986+LucianoVelasquez@users.noreply.github.com>
2024-04-20 17:00:34 -0300

    fexed 3

--- app/clientes/update/page.tsx
+++ app/clientes/update/page.tsx
@@ -1,5 +1,5 @@
 import Home from "@/components/Home/home";
-import UserUpdate from "@/Components/Home/userupdate";
+import UserUpdate from "@/components/Home/userupdate";
 
 export default function Page(){
   return(
